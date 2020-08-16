b063126ac91e35c6b95b60d37a3daaa28f3733e3
Luís Miguel Duarte Conceição <fc48303@alunos.fc.ul.pt>
2020-08-16 20:43:32 +0100

    Created landing page, added images and image slider

--- src/utils/scripts.js
+++ src/utils/scripts.js
@@ -1,1 +1,34 @@
-var abc = 0;
+$(document).ready(function(){
+  $(".happy").click(function(){
+    $(".happy").fadeOut();
+    $("#side_author").animate({ opacity: 1 });
+    $( "#main_section").animate({ opacity: 1 });
+
+  });
+});
+
+function navigation(direction) {
+  let img = document.getElementById("photography");
+  let current_img = img.getAttribute('data-img');
+  if (direction === "previous") {
+    if(parseInt(current_img) > 1) {
+      current_img--;
+      img.src='images/' + current_img + '.png';
+      img.setAttribute("data-img", current_img);
+    }else {
+      current_img = 25;
+      img.src='images/' + current_img + '.png';
+      img.setAttribute("data-img", current_img);
+    }
+  }else if (direction === "next") {
+    if(parseInt(current_img) !== 25) {
+      current_img++;
+      img.src='images/' + current_img + '.png';
+      img.setAttribute("data-img", current_img);
+    }else {
+      current_img = 1;
+      img.src='images/' + current_img + '.png';
+      img.setAttribute("data-img", current_img);
+    }
+  }
+}
