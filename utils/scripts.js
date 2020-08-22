67429b9058d1d5f883e15a16515414b1ad51c2d4
Luís Conceição <48288080+luisconceicaodev@users.noreply.github.com>
2020-08-22 16:59:26 +0100

    Testing optimized images

---
+++ utils/scripts.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+  $(".happy").click(function(){
+    $(".happy").fadeOut();
+    $("#side_author").animate({ opacity: 1 });
+    $( "#main_section").animate({ opacity: 1 });
+
+  });
+});
+
+function navigation(direction) {
+  let img = document.getElementById("photography");
+  let current_img = img.getAttribute('data-img');
+  if (direction === "previous") {
+    if(parseInt(current_img) > 1) {
+      current_img--;
+      img.src='images/' + current_img + '-min.png';
+      img.setAttribute("data-img", current_img);
+    }else {
+      current_img = 5;
+      img.src='images/' + current_img + '-min.png';
+      img.setAttribute("data-img", current_img);
+    }
+  }else if (direction === "next") {
+    if(parseInt(current_img) !== 5) {
+      current_img++;
+      img.src='images/' + current_img + '-min.png';
+      img.setAttribute("data-img", current_img);
+    }else {
+      current_img = 1;
+      img.src='images/' + current_img + '-min.png';
+      img.setAttribute("data-img", current_img);
+    }
+  }
+}
