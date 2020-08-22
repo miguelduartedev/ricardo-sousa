ffabf0d8a04824a9df86232827d5083b9029f7c1
Luís Conceição <48288080+luisconceicaodev@users.noreply.github.com>
2020-08-22 17:17:42 +0100

    Image slider ver:1.2

--- src/utils/scripts.js
+++ src/utils/scripts.js
@@ -10,31 +10,24 @@ $(document).ready(function(){
 function navigation(direction) {
   let img = document.getElementById("photography");
   let current_img = img.getAttribute('data-img');
-  let array = {
-    "1": "https://imgur.com/33LJH1l.png",
-    "2": "https://imgur.com/4LQb8YV.png",
-    "3": "https://imgur.com/hCfMt9m.png",
-    "4": "https://imgur.com/6VCSQBV.png",
-    "5": "https://imgur.com/zVj77ea.png",
-  }
   if (direction === "previous") {
     if(parseInt(current_img) > 1) {
       current_img--;
-      img.src=array[current_img];
+      img.src='images/' + current_img + '-min.png';
       img.setAttribute("data-img", current_img);
     }else {
       current_img = 5;
-      img.src=array[current_img];
+      img.src='images/' + current_img + '-min.png';
       img.setAttribute("data-img", current_img);
     }
   }else if (direction === "next") {
     if(parseInt(current_img) !== 5) {
       current_img++;
-      img.src=array[current_img];
+      img.src='images/' + current_img + '-min.png';
       img.setAttribute("data-img", current_img);
     }else {
       current_img = 1;
-      img.src=array[current_img];
+      img.src='images/' + current_img + '-min.png';
       img.setAttribute("data-img", current_img);
     }
   }
