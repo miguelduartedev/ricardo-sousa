4b91d944fbb4ff6525f5c5055b08fdf6c1eaea09
Luís Conceição <48288080+luisconceicaodev@users.noreply.github.com>
2020-10-18 17:29:47 +0100

    Morphed Read Me, Info into Single Page (smoother animations)

--- src/utils/scripts.js
+++ src/utils/scripts.js
@@ -2,7 +2,7 @@ console.log('Developed by Luís Conceição: https://www.linkedin.com/in/lu%C3%A
 
 $(document).ready(function () {
 
-  function preload(imageArray, index) {
+  /* function preload(imageArray, index) {
     index = index || 0;
     if (imageArray && imageArray.length > index) {
         var img = new Image ();
@@ -11,7 +11,7 @@ $(document).ready(function () {
         }
         img.src = preLoad[index];
     }
-  }
+  } */
   /* images is an array with image metadata */
   //preload(preLoad);
 
@@ -22,7 +22,7 @@ $(document).ready(function () {
     imgInDOM = document.querySelector('[data-img =' + img + ']');
     imgInDOM.style.opacity = 0.3; 
     description.innerHTML = imgInDOM.dataset.description;
-    $(".happy").hide();
+    $(".landing").hide();
     $("#author").hide();
     $("#top_menu").animate({ opacity: 1 });
     project_handler(imgInDOM);
@@ -40,8 +40,8 @@ $(document).ready(function () {
     imgInDOM.animate({ opacity: 1 });
     description.innerHTML = imgInDOM.dataset.description;
     //$("#description").html(images[1]['description']);
-    $(".happy").click(function () {
-      $(".happy").fadeOut();
+    $(".landing").click(function () {
+      $(".landing").fadeOut();
       $("#author").fadeOut();
       $("#top_menu").animate({ opacity: 1 });
       $(".-active").animate({ opacity: 1 });
@@ -73,8 +73,6 @@ document.onkeydown = function (event) {
 function navigation(direction) {
   let img = document.querySelector(".-active");
   let description = document.getElementById("description");
-  let read = document.getElementById("read");
-  let counter = document.getElementById("counter");
   let current_img = img.getAttribute('data-img').split('-')[1];
   if (direction === "previous") {
     if (parseInt(current_img) > 1) {
@@ -113,10 +111,38 @@ function navigation(direction) {
 
 function project_handler(nextImgDOM) {
   try {
+    let read = document.querySelector('[data-reference=index]');
+    let counter = document.getElementById("counter");
     return nextImgDOM.dataset.project === 'true' ?
     (counter.innerHTML = nextImgDOM.dataset.position, counter.style.visibility = 'visible', read.style.visibility = 'visible') : (counter.style.visibility = 'hidden', read.style.visibility = 'hidden');
   } catch {
   }
+}
+
+function mainToReadMe() {
+  $('.firstFade').fadeOut('fast');
+  $('#main_section').animate({marginTop: '-300%'}, 'fast').fadeOut('slow');
+  $('.imagesLink').fadeIn('fast');
+  $('#readme').fadeIn('fast').animate({marginTop: '0'}, 300);
+}
+
+function readMeToMain() {
+  $('.imagesLink').fadeOut('fast');
+  $('#readme').animate({marginTop: '500%'}, 'fast').fadeOut('fast');
+  $('#main_section').fadeIn('fast').animate({marginTop: '0'}, 'normal');
+  $('.firstFade').fadeIn('fast');
+}
+
+function mainToInfo() {
+  $('.firstFade').fadeOut('fast');
+  $('#main_section').fadeOut('fast');
+  $('#info').fadeIn('fast');
+}
+
+function infoToMain() {
+  $('#info').fadeOut('fast');
+  $('#main_section').fadeIn('fast');
+  $('.firstFade').fadeIn('fast');
 }
 
 function mobile_nav() {
@@ -156,3 +182,72 @@ function fadeOutBody() {
     $("body").fadeOut(1000);
   });
 }
+
+// READ ME 
+
+function textHandler() {
+  let project = document.querySelector(".-active").dataset.projectname;
+  document.getElementById('readme_title').innerHTML = readme_handler[project]['header'];
+  document.getElementById('readme_description').innerHTML = readme_handler[project]['description'];
+}
+
+const heavy_water_header = 'Heavy Water <br> Ricardo Sousa';
+
+const heavy_water_description = `An essay exploring how mechanical complications allow mundane objects perform seemlessly, quitely while being very intuitive. <br><br>
+With an obsessive attention to detail, specially on the inside, performance and durability, this project has resulted into an object that provides and intruiging and almost magical performance. <br><br>
+After two years of development Ensaio 304 is a commercially available faucet, writing a statment regarding the importance of hidden complex intricacies remiding us about the value of tactality.`;
+
+const silent_gestures_header = 'Silent gestures <br> Ricardo Sousa and André Duarte';
+
+const silent_gestures_description = `Silent gestures started with an uncompromised observation of objects from different
+time periods acrossthe
+portuguese cultural chain from continental Portugal to its colonies at the Lisbon’s Museum of Ethnology. The
+intent was simple yet quite complex, to understand what objects stopped being produced over time, understand
+why and if possible present a contemporary perspective that could bring them back in a way that the
+contemporary market narratives could embrace them based on the aesthetic, ethic and cultural values.
+<br /><br />
+
+During that process the we understood the time specific values of the objects and because of that, bringing
+them back based on a revival whim would not create value. However, the traditional techniques weakened with
+time could bring intrinsic cultural knowledge back, and by exposing them in a raw yet very eclectic manner
+they could be broadly understood and deeply appreciated.<br /><br />
+
+We were immensely attracted by the burners and amulets, objects with ritualistic connotations, some of them
+very intimate and personal. This was the turning point, the time where typology and technique collided and the
+idea was born. We took fragrance diffusers as a mean to glorify traditional linen, the chosen material due to
+its ability to hold liquids yet allowing them to breath.<br /><br />
+
+The design process was headed to the conception of sculptural volumes, each one resembling an hyperbolic
+spatial expansion due to its scale, not due to a necessity or the need to achieve an evolved version but
+rather to present a contemporary perspective on their aesthetic, meaning as objects, an evaluation for the use
+of traditional materials and techniques in the contemporary context, and ultimately the role of design as an
+agent for the creation of matter in a world where immateriality seems to be the ultimate vanishing
+point.<br /><br />
+
+&nbsp; &nbsp; Who strive - you don’t know how the others strive <br>
+&nbsp; &nbsp; To paint a little thing like that you smeared<br>
+&nbsp; &nbsp; Carelessly passing with your robes afloat,<br>
+&nbsp; &nbsp; Yet do much less, so much less, Someone says,<br>
+&nbsp; &nbsp; (I know his name, no matter) - so much less!<br>
+&nbsp; &nbsp; Well, less is more (...)<br /><br />
+
+In the poem entitled Andrea del Sarto, written in 1855 by Robert Browning we can read the so acclaimed
+expression Less is More. This collection does not intend to create a better version of the objects per se,
+ and because of that it does deviate from the traditional academical approach to design. However, if design
+ is intention (Flusser 1993) we may comfortably assert that an object can also be a medium for a message. And
+that’s what we intended with each piece, something explicit in the way we made all the components visible,
+exposing the mechanism of each part and the symbiotic relations between them and the object  as a
+whole.<br /><br />
+
+In the Diffuser 2 specifically, theres is a process deconstruction by showing the four wires of traditional
+linen yarn individually before they become into a cord made from a traditional portuguese technique called
+bilros. Records of structure exposition are also clear in Diffuser 1 and Estrutura Vertical.<br /><br />
+
+In the Diffuser 2 specifically, theres is a process deconstruction by showing the four wires of traditional
+linen yarn individually before they become into a cord made from a traditional portuguese technique called
+bilros. Records of structure exposition are also clear in Diffuser 1 and Estrutura Vertical.`;
+
+var readme_handler = {
+  'Heavy water': { 'header': heavy_water_header, 'description': heavy_water_description},
+  'Silent gestures': { 'header': silent_gestures_header, 'description': silent_gestures_description},
+  };
